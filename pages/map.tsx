9775798dea5705c622409966fdbff9b78989a564
Vaastav Arora <arora74@purdue.edu>
2020-07-17 11:00:50 -0400

    Packages added

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,8 +1,15 @@
-import Layout from '../src/features/common/Layout'
+import Layout from '../src/features/common/Layout';
+import { Map as M,TileLayer, Popup, Marker} from 'react-leaflet';
 
 export default function Map() {
   return (
     <Layout>
+      <M className="map">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+      </M>
       <p>Map</p>
     </Layout>
   )
