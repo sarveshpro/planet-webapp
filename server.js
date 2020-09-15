bafe87384557ed313e845b84429d49ce266763b8
Sarvesh Warge <sarveshwarge@gmail.com>
2020-09-15 11:30:26 +0530

    if https don't redirect

--- server.js
+++ server.js
@@ -32,14 +32,15 @@ if (!dev && cluster.isMaster) {
     if (!dev) {
       // Enforce SSL & HSTS in production
       server.use(function (req, res, next) {
-        // var proto = req.headers['x-forwarded-proto'];
-        // if (proto === 'https') {
-        // res.set({
-        //   'Strict-Transport-Security': 'max-age=31557600', // one-year
-        // });
-        return next();
-        // }
-        // res.redirect('https://' + req.headers.host + req.url);
+        var proto = req.headers['x-forwarded-proto'];
+        if (proto === 'https') {
+          res.set({
+            'Strict-Transport-Security': 'max-age=31557600', // one-year
+          });
+          return next();
+        } else {
+          res.redirect('https://' + req.headers.host + req.url);
+        }
       });
     }
 
